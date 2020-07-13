48aa9638007dc081f300ab4f7b741cac89e2df51
Sven <sven@berger-lembeck.de>
2020-07-13 14:04:30 +0200

    Nightly Hinzugefügt

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker build -t derbrummbaer/rpi-pilight:jessie  ./jessie
-docker build -t derbrummbaer/rpi-pilight:stretch ./stretch
-docker build -t derbrummbaer/rpi-pilight:buster  ./buster
-docker build -t derbrummbaer/rpi-pilight:latest  ./latest
+#docker build -t derbrummbaer/rpi-pilight:jessie  ./jessie
+#docker build -t derbrummbaer/rpi-pilight:stretch ./stretch
+#docker build -t derbrummbaer/rpi-pilight:stretch-nightly ./stretch-nightly
+#docker build -t derbrummbaer/rpi-pilight:buster  ./buster
+docker build -t derbrummbaer/rpi-pilight:buster-nightly  ./buster-nightly
+#docker build -t derbrummbaer/rpi-pilight:latest  ./latest
