7a6dfe66270be8272f6b4b290ce1963450bac98e
Sven <sven@berger-lembeck.de>
2020-07-13 12:32:23 +0200

    Änderung: Löschen PID File vor Start

---
+++ stretch nightly/scripte/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Wenn pidfile nicht gelöscht wurde
+if [ -d /var/run/pilight.pid) ]; then
+	rm /var/run/pilight.pid
+fi
+# Wenn Config Datei noch nicht exitstiert, -->kopieren
+if ! [ "$(ls -A /etc/pilight/config.bak)" ]; then
+	cp -a /etc/pilight.config/config /etc/pilight/config.bak
+fi
+
+# Kopieren der Konfigurationsdatei
+/bin/cp /etc/pilight/config.bak /etc/pilight/config.json
+# Starten des Pilight Daemon
+/usr/local/sbin/pilight-daemon -F
